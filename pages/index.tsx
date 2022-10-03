5d59b9b527c872233db20e4e090e741752df5f0b
James Hancock <james.hancock@torchbox.com>
2022-10-03 10:25:02 +0100

    simplify content structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.scss";
-import homePageContent from "../data/homepage.json";
+import content from "../data/website.json";
 import Header from "../components/Header";
 
 const Home: NextPage = () => {
@@ -18,9 +18,9 @@ const Home: NextPage = () => {
 
       <Header />
       <main className={styles.main}>
-        <h1 className={styles.title}>{homePageContent.title}</h1>
+        <h1 className={styles.title}>{content.title}</h1>
 
-        <p className={styles.description}>{homePageContent.description}</p>
+        <p className={styles.description}>{content.description}</p>
       </main>
     </div>
   );
