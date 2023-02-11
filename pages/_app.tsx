98cd8e7f17bb716ffe6bcbfbed10eaee4dff2b13
James Hancock <james.hancock@torchbox.com>
2023-02-11 21:29:02 +0000

    add new font to the site

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
-import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import localFont from "@next/font/local";
 import settings from "../data/settings.json";
 import { SettingsProvider } from "context/settings";
 import { EditorProvider } from "context/editor";
 import { useImmer } from "use-immer";
+import Header from "components/Header";
+import Footer from "components/Footer";
+import "../styles/globals.scss";
+
+// From the Indian Font Foundry: https://www.fontshare.com/fonts/sentient
+const sentient = localFont({ src: "./Sentient-Variable.ttf" });
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [editorContents, setEditorContents] = useImmer({ data: "" });
@@ -12,7 +18,16 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SettingsProvider settings={settings}>
       <EditorProvider data={value}>
-        <Component {...pageProps} />
+        <style jsx global>{`
+          html {
+            font-family: ${sentient.style.fontFamily};
+          }
+        `}</style>
+        <div className={"container"}>
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </div>
       </EditorProvider>
     </SettingsProvider>
   );
