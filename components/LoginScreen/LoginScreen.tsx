c04bb2eba1ebe14cc0ddf375fecf119d86461c7b
James Hancock <james.hancock@torchbox.com>
2023-02-21 12:17:55 +0000

    update login screen page

--- components/LoginScreen/LoginScreen.tsx
+++ components/LoginScreen/LoginScreen.tsx
@@ -4,7 +4,6 @@ import styles from "./LoginScreen.module.scss";
 
 export const LoginScreen = () => (
   <div className={styles.container}>
-    <h1 className={styles.title}>Administrator View</h1>
     <p className={styles.notice}>
       This page requires authentication to continue.
     </p>
